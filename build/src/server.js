13975b8262c8c388b9b35fb48f4538d34d1da5b8
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-19 12:12:12 +0100

    feat: added many features, public/scripts, public/styles, public/views

---
+++ build/src/server.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello Competition');
+});
+app.listen(port, () => {
+    console.log('Server is listening on port ' + port);
+});
--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import { Request, Response, } from "express";
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path')
+
+app.use(express.static('public'));
 
 app.get('/', (req:Request, res:Response) => {
-    res.send('Hello Competition')
+    res.sendFile(path.join(__dirname, '../public', 'index.html'))
 });
 
 app.listen(port, () => {
