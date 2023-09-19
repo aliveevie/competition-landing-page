65d951d58ee80a966a8136bf8eb874ee830d1950
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-19 11:12:46 +0100

    feat: add the server.ts

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { Request, Response, } from "express";
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Hello Competition')
+});
+
+app.listen(port, () => {
+    console.log('Server is listening on port ' + port)
+});
