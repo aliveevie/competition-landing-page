5ae7c558d280c7277740906edacee2a8bbb384d8
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-09-20 09:50:24 +0100

    feat: modified files

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -10,6 +10,27 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
+const qaElements = document.querySelectorAll('.qa');
+
+qaElements.forEach(function(qaElement) {
+    const answerElement = qaElement.querySelector('.ans');
+    const iconElement = qaElement.querySelector('.answer');
+
+    qaElement.addEventListener('click', function() {
+        if (answerElement.style.display === 'block') {
+            answerElement.style.display = 'none';
+            iconElement.classList.remove('fa-minus');
+            iconElement.classList.add('fa-plus');
+        } else {
+            answerElement.style.display = 'block';
+            iconElement.classList.remove('fa-plus');
+            iconElement.classList.add('fa-minus');
+        }
+    });
+});
+
+
+
     
 });
 
